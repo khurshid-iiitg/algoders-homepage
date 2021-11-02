5c80c62cccaa609da5787af5d02f9d2074fdfd27
Khurshid Alam Bhuyan <khurs10101@gmail.com>
2021-11-02 20:08:49 +0530

    added deffer to all

--- index.php
+++ index.php
@@ -748,32 +748,32 @@ if (isset($_POST['submit'])) {
 		</footer>
 
 		<!-- Jquery JS -->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/jquery-migrate-3.0.0.js"></script>
+		<script defer src="js/jquery.min.js"></script>
+		<script defer src="js/jquery-migrate-3.0.0.js"></script>
 		<!-- Popper JS -->
-		<script src="js/popper.min.js"></script>
+		<script defer src="js/popper.min.js"></script>
 		<!-- Bootstrap JS -->
-		<script src="js/bootstrap.min.js"></script>
+		<script defer src="js/bootstrap.min.js"></script>
 		<!-- Modernizr JS -->
-		<script src="js/modernizr.min.js"></script>
+		<script defer src="js/modernizr.min.js"></script>
 		<!-- ScrollUp JS -->
-		<script src="js/scrollup.js"></script>
+		<script defer src="js/scrollup.js"></script>
 		<!-- FacnyBox JS -->
-		<script src="js/jquery-fancybox.min.js"></script>
+		<script defer src="js/jquery-fancybox.min.js"></script>
 		<!-- Cube Portfolio JS -->
-		<script src="js/cubeportfolio.min.js"></script>
+		<script defer src="js/cubeportfolio.min.js"></script>
 		<!-- Slick Nav JS -->
-		<script src="js/slicknav.min.js"></script>
+		<script defer src="js/slicknav.min.js"></script>
 		<!-- Slick Nav JS -->
-		<script src="js/slicknav.min.js"></script>
+		<script defer src="js/slicknav.min.js"></script>
 		<!-- Slick Slider JS -->
-		<script src="js/owl-carousel.min.js"></script>
+		<script defer src="js/owl-carousel.min.js"></script>
 		<!-- Easing JS -->
-		<script src="js/easing.js"></script>
+		<script defer src="js/easing.js"></script>
 		<!-- Magnipic Popup JS -->
-		<script src="js/magnific-popup.min.js"></script>
+		<script defer src="js/magnific-popup.min.js"></script>
 		<!-- Active JS -->
-		<script src="js/active.js"></script>
+		<script defer src="js/active.js"></script>
 </body>
 
 </html>
